a6601750cf23dba97b27bbc4863d092bf5a4f469
진성현 <jinsunghyun9@gmail.com>
2024-04-05 09:57:28 +0900

    accounts views.py 수정

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.views import (
+    LoginView,
+)
 
-# Create your views here.
+
+class UserLoginView(LoginView):
+    template_name = "accounts/login.html"
+    redirect_authenticated_user = (
+        True  # 이미 인증된 사용자는 success_url로 리디렉션합니다.
+    )
+
+    def get_success_url(self):
+        # return reverse_lazy('post_list')  # 'user_profile'은 로그인 성공 후 리디렉션할 URL 이름입니다.
+        pass
+
+
+user_login = UserLoginView.as_view()
--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("facechats/", include("facechats.urls")),
+    path("posts/", include("posts.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
