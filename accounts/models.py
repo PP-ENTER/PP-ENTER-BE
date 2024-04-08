95fe8bef96c7ae3139e9fd4b87d26a123447a897
SanghyunPark <shpark@mv-w.com>
2024-04-08 22:59:42 +0900

    ♻ REFACTOR: Profile 모델 수정

--- accounts/models.py
+++ accounts/models.py
@@ -12,11 +12,16 @@ class CustomUser(AbstractUser):
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)  # CustomUser와 1:1 관계
     # primary_key를 CustomUser의 pk로 설정하여 통합적으로 관리
     nickname = models.CharField(max_length=255, unique=False)  # 닉네임
-    profile_image = models.ImageField(upload_to='profile/', default='default.png')  # 프로필 이미지
+    profile_image = models.ImageField(upload_to='profile/', null=True, blank=True)  # 프로필 이미지
+    first_name = models.CharField(max_length=255, null=True, blank=True)  # 이름
+    last_name = models.CharField(max_length=255, null=True, blank=True)  # 성
     updated_at = models.DateTimeField(auto_now=True)  # 수정일
+
+    def __str__(self):
+        return self.nickname
 
 
 @receiver(post_save, sender=CustomUser)
