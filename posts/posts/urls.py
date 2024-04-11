4cfe2b47d5e774233a73fe919d0c8342f10ca35c
Eileen <leh7875@naver.com>
2024-04-11 16:05:00 +0900

    ♻ REFACTOR 클래스명 수정

--- posts/posts/urls.py
+++ posts/posts/urls.py
@@ -1,24 +1,24 @@
 from django.urls import path
 from .views import (
-    PhotoListCreateView, PhotoRetrieveUpdateDeleteView,
-    LikeCreateView, LikeDeleteView,
-    FavoriteCreateView, FavoriteDeleteView,
-    CommentCreateView, CommentUpdateDeleteView,
-    TagListCreateView, PhotoTagCreateView, PhotoTagDeleteView
+    PhotoListCreateView, PhotoRetrieveUpdateDestroyView,
+    LikeCreateView, LikeDestroyView,
+    FavoriteCreateView, FavoriteDestroyView,
+    CommentCreateView, CommentUpdateDestroyView,
+    TagListCreateView, PhotoTagCreateView, PhotoTagDestroyView
 )
 
 app_name = 'posts'
 
 urlpatterns = [
     path('photos/', PhotoListCreateView.as_view(), name='photo_list_create'),
-    path('photos/<int:pk>/', PhotoRetrieveUpdateDeleteView.as_view(), name='photo_detail'),
+    path('photos/<int:pk>/', PhotoRetrieveUpdateDestroyView.as_view(), name='photo_detail'),
     path('likes/', LikeCreateView.as_view(), name='like_create'),
-    path('likes/<int:pk>/', LikeDeleteView.as_view(), name='like_delete'),
+    path('likes/<int:pk>/', LikeDestroyView.as_view(), name='like_delete'),
     path('favorites/', FavoriteCreateView.as_view(), name='favorite_create'),
-    path('favorites/<int:pk>/', FavoriteDeleteView.as_view(), name='favorite_delete'),
+    path('favorites/<int:pk>/', FavoriteDestroyView.as_view(), name='favorite_delete'),
     path('comments/', CommentCreateView.as_view(), name='comment_create'),
-    path('comments/<int:pk>/', CommentUpdateDeleteView.as_view(), name='comment_update_delete'),
+    path('comments/<int:pk>/', CommentUpdateDestroyView.as_view(), name='comment_update_delete'),
     path('tags/', TagListCreateView.as_view(), name='tag_list_create'),
     path('photo_tags/', PhotoTagCreateView.as_view(), name='photo_tag_create'),
-    path('photo_tags/<int:pk>/', PhotoTagDeleteView.as_view(), name='photo_tag_delete'), 
+    path('photo_tags/<int:pk>/', PhotoTagDestroyView.as_view(), name='photo_tag_delete'), 
 ]
