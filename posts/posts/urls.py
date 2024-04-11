0b44520722438b16bae16c734689d4bd6721b4a3
Eileen <leh7875@naver.com>
2024-04-11 12:17:31 +0900

    ✨ FEAT: urls.py 작성

--- posts/posts/urls.py
+++ posts/posts/urls.py
@@ -1,6 +1,24 @@
 from django.urls import path
-from . import views
+from .views import (
+    PhotoListCreateView, PhotoRetrieveUpdateDeleteView,
+    LikeCreateView, LikeDeleteView,
+    FavoriteCreateView, FavoriteDeleteView,
+    CommentCreateView, CommentUpdateDeleteView,
+    TagListCreateView, PhotoTagCreateView, PhotoTagdeleteView
+)
+
+app_name = 'posts'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('photos/', PhotoListCreateView.as_view(), name='photo_list_create'),
+    path('photos/<int:pk>/', PhotoRetrieveUpdateDeleteView.as_view(), name='photo_detail'),
+    path('likes/', LikeCreateView.as_view(), name='like_create'),
+    path('likes/<int:pk>/', LikeDeleteView.as_view(), name='like_delete'),
+    path('favorites/', FavoriteCreateView.as_view(), name='favorite_create'),
+    path('favorites/<int:pk>/', FavoriteDeleteView.as_view(), name='favorite_delete'),
+    path('comments/', CommentCreateView.as_view(), name='comment_create'),
+    path('comments/<int:pk>/', CommentUpdateDeleteView.as_view(), name='comment_update_delete'),
+    path('tags/', TagListCreateView.as_view(), name='tag_list_create'),
+    path('photo_tags/', PhotoTagCreateView.as_view(), name='photo_tag_create'),
+    path('photo_tags/<int:pk>/', PhotoTagDeleteView.as_view(), name='photo_tag_delete'),
 ]
