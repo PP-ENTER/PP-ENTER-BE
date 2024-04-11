4dcfe2a26288c3c84976fc5b24f2887570addcf8
chul <dudcjf3232@gmail.com>
2024-04-11 23:28:17 +0900

    ✨ FEAT: : 세부 페이지 기능 추가

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import (
-    PostMainListView,
+    PostMainListView, PostDetailListView
 )
 
 urlpatterns = [
     path('posts_main_list/', PostMainListView.as_view(), name='posts_main_list'),
+    path('posts_detail_list/<int:userid>/', PostDetailListView.as_view(), name='posts_detail_list'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -12,3 +12,13 @@ class PostMainListView(generics.ListAPIView):
 
     def get_queryset(self):
         return Photo.objects.all().order_by('-created_at')[:10]
+    
+class PostDetailListView(generics.ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs.get('userid')
+        if user_id == 0:  # '-1'이 전달되면 전체 포스트 목록 반환
+            return Photo.objects.all()
+        else:  # 그렇지 않으면 해당 'userid'의 포스트만 필터링
+            return Photo.objects.filter(author_id=user_id)
