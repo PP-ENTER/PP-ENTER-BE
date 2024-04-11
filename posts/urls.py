35b8202a47ba908a6db7609c4f4ba646467efd08
chul <dudcjf3232@gmail.com>
2024-04-12 01:14:44 +0900

    ✨ FEAT : 세부 페이지 검색기능 추가

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (
-    PostMainListView, PostDetailListView
+    PostMainListView, PostDetailListView, PostMainListSearchView, PostDetailListSearchView
 )
 
 urlpatterns = [
     path('posts_main_list/', PostMainListView.as_view(), name='posts_main_list'),
     path('posts_detail_list/<int:userid>/', PostDetailListView.as_view(), name='posts_detail_list'),
+    path('posts_main_list_search/<str:photo_name>/', PostMainListSearchView.as_view(), name='posts_main_list_search'),
+    path('posts_detail_list_search/<str:photo_name>/', PostDetailListSearchView.as_view(), name='posts_detail_list_search'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -4,7 +4,7 @@ from .models import Photo
 from .serializers import (
     PostSerializer
 )
-
+from django.db.models import Q
 
 
 class PostMainListView(generics.ListAPIView):
@@ -19,6 +19,25 @@ class PostDetailListView(generics.ListAPIView):
     def get_queryset(self):
         user_id = self.kwargs.get('userid')
         if user_id == 0:  # '-1'이 전달되면 전체 포스트 목록 반환
-            return Photo.objects.all()
+            return Photo.objects.all().order_by('-created_at')
         else:  # 그렇지 않으면 해당 'userid'의 포스트만 필터링
-            return Photo.objects.filter(author_id=user_id)
+            return Photo.objects.filter(author_id=user_id).order_by('-created_at')
+
+
+class PostMainListSearchView(generics.ListAPIView):
+    serializer_class = PostSerializer
+ 
+    def get_queryset(self):
+        # photo_name = self.request.query_params.get('photo_name', None)
+        photo_name = self.kwargs['photo_name']
+
+        return Photo.objects.all().filter(Q(photo_name__icontains = photo_name)).order_by('-created_at')[:10]
+    
+class PostDetailListSearchView(generics.ListAPIView):
+    serializer_class = PostSerializer
+ 
+    def get_queryset(self):
+        # photo_name = self.request.query_params.get('photo_name', None)
+        photo_name = self.kwargs['photo_name']
+
+        return Photo.objects.all().filter(Q(photo_name__icontains = photo_name)).order_by('-created_at')
