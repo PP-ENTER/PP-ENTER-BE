c945e5151c36bf6301be8af9403bacd48f1052f0
Eileen <leh7875@naver.com>
2024-04-11 17:02:20 +0900

    ♻ REFACTOR: photo->post 클래스명 변경

--- posts/posts/admin.py
+++ posts/posts/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Photo, Like, Favorite, Comment, Tag, PhotoTag
+from .models import Post, Like, Favorite, Comment, Tag, PhotoTag
 
 # 관리자 페이지에서 모델 데이터를 보기 좋게 표시하기 위한 클래스
-class PhotoAdmin(admin.ModelAdmin):
+class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'user_id', 'content', 'created_at', 'updated_at')
     list_filter = ('created_at', 'updated_at')
     search_fields = ('content', 'user_id', 'photo_tag')
@@ -18,7 +18,7 @@ class CommentAdmin(admin.ModelAdmin):
     list_filter = ('created_at', 'updated_at')
     search_fields = ('content')
 
-admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Like, LikeAdmin)
 admin.site.register(Favorite, FavoriteAdmin)
 admin.site.register(Comment, CommentAdmin)
