fd3efb0102ba4385c8edc9f0f90e76abd7ebcd61
chul <dudcjf3232@gmail.com>
2024-04-12 18:11:17 +0900

    ♻️ REFACTOR : 화상채팅 기능 확정

--- config/settings.py
+++ config/settings.py
@@ -170,11 +170,9 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Channels
 ASGI_APPLICATION = 'config.asgi.application'
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
+
+CHANNEL_LAYERS={
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+     }
 }
--- facechats/consumers.py
+++ facechats/consumers.py
@@ -197,3 +197,47 @@ class CallConsumer(WebsocketConsumer):
             'type': 'ICEcandidate',
             'data': event['data']
         }))
+
+
+
+# class ChatConsumer(AsyncWebsocketConsumer):
+#     async def connect(self):
+#         self.room_group_name = 'chat_room'
+
+#         # Join room group
+#         await self.channel_layer.group_add(
+#             self.room_group_name,
+#             self.channel_name
+#         )
+
+#         await self.accept()
+
+#     async def disconnect(self, close_code):
+#         # Leave room group
+#         await self.channel_layer.group_discard(
+#             self.room_group_name,
+#             self.channel_name
+#         )
+
+#     async def receive(self, text_data):
+#         data = json.loads(text_data)
+#         if data['type'] == 'invite':
+#             # Process invite
+#             await self.channel_layer.group_send(
+#                 self.room_group_name,
+#                 {
+#                     'type': 'chat_invite',
+#                     'room_id': data['room_id'],
+#                     'invitee': data['invitee'],
+#                     'inviter': data['inviter']
+#                 }
+#             )
+
+#     # Handler for invite messages
+#     async def chat_invite(self, event):
+#         await self.send(text_data=json.dumps({
+#             'type': 'invite',
+#             'room_id': event['room_id'],
+#             'invitee': event['invitee'],
+#             'inviter': event['inviter']
+#         }))
